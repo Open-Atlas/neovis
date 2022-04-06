417c23347c4b750d6ece153d170a45ff648a3df0
RayRemnant <rayremnant.me@gmail.com>
2022-04-06 14:51:04 +0300

    2 files modified, 3 added

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-// run `node index.js` in the terminal
+const Koa = require('koa');
+const Router = require('@koa/router');
+const helmet = require('koa-helmet');
+const cors = require('@koa/cors');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = new Koa();
+const router = new Router();
+
+app.use(cors());
+app.use(helmet());
+
+const { createReadStream } = require('fs');
+
+router.get('/', (ctx) => {
+  ctx.type = 'html';
+  ctx.body = createReadStream('neovis.html');
+});
+
+router.use((ctx) => {
+  ctx.response.status = 404;
+});
+
+app.use(router.routes());
+
+app.on('error', (e) => {
+  // headers data makes Koa crash during error handling
+  e.headers = {};
+  console.error(e);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {});
+console.log('listening on port ' + port);
